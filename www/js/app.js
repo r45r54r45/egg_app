4bb45f56237d5d114e488d6b7b01157313fe366e
Woohyun Kim <r45r54r45@naver.com>
2016-08-01 01:21:15 +0900

    init

---
+++ www/js/app.js
@@ -0,0 +1,119 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.services', 'rzModule'])
+
+  .run(function ($ionicPlatform, $state) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl'
+      })
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.class', {
+        url: '/class',
+        views: {
+          'tab-class': {
+            templateUrl: 'templates/tab-class.html',
+            controller: 'ClassCtrl'
+          }
+        }
+      })
+      .state('tab.class-detail', {
+        url: '/class/:classId',
+        views: {
+          'tab-class': {
+            templateUrl: 'templates/class-detail.html',
+            controller: 'ClassDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.notice', {
+        url: '/notice',
+        views: {
+          'tab-notice': {
+            templateUrl: 'templates/tab-notice.html',
+            controller: 'NoticeCtrl'
+          }
+        }
+      })
+      .state('tab.board', {
+        url: '/board',
+        views: {
+          'tab-board': {
+            templateUrl: 'templates/tab-board.html',
+            controller: 'BoardCtrl'
+          }
+        }
+      })
+
+      .state('tab.mypage', {
+        url: '/mypage',
+        views: {
+          'tab-mypage': {
+            templateUrl: 'templates/tab-mypage.html',
+            controller: 'MypageCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise(function () {
+      var logged = window.localStorage.isLogined;
+      console.log("check logined",logged);
+      // Check User logined or not
+      if (logged != "true") {
+        console.log("login");
+        return 'login';
+      } else {
+        console.log("class");
+        return 'tab/class';
+      }
+    });
+
+
+  })
+  .directive('ratingInfo', function () {
+    return {
+      // Restrict it to be an attribute in this case
+      restrict: 'A',
+      // responsible for registering DOM listeners as well as updating the DOM
+      link: function (scope, element, attrs) {
+        $(element).barrating(scope.$eval(attrs.ratingInfo));
+      }
+    };
+  });
---
+++ www/js/controllers.js
@@ -0,0 +1,129 @@
+angular.module('starter.controllers', [])
+
+  .controller('ClassCtrl', function ($scope, $ionicModal, $ionicLoading) {
+    $ionicModal.fromTemplateUrl('templates/modal/class-sort.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal_sort = modal;
+    });
+    $ionicModal.fromTemplateUrl('templates/modal/class-add.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal_add = modal;
+    });
+
+    $scope.openModal = function (type) {
+      if (type === "sort") {
+        $scope.modal_sort.show();
+      } else {
+
+        $scope.modal_add.show();
+      }
+    };
+    $scope.closeModal = function (type) {
+      if (type === "sort") {
+        $scope.modal_sort.hide();
+      } else {
+        $scope.modal_add.hide();
+      }
+    };
+    $scope.showLoading = function () {
+      $ionicLoading.show({
+        template: 'Loading...'
+      }).then(function () {
+        console.log("The loading indicator is now displayed");
+      });
+    };
+    $scope.hideLoading = function () {
+      $ionicLoading.hide().then(function () {
+        console.log("The loading indicator is now hidden");
+      });
+    };
+
+    $scope.priceSlider = {
+      value: 0,
+      options: {
+        showTicks: true,
+        floor: 0,
+        ceil: 5,
+        minLimit: 0,
+        maxLimit: 5,
+        hidePointerLabels: true,
+        hideLimitLabels: true
+      }
+    };
+
+
+  })
+  .controller('NoticeCtrl', function ($scope) {
+  })
+
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
+
+  .controller('ClassDetailCtrl', function ($scope, $stateParams) {
+    // $scope.chat = Chats.get($stateParams.chatId);
+    console.log($stateParams.classId);
+    $scope.loadMore = function () {
+      console.log("class detail", "load more");
+      setTimeout(function () {
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      }, 3000);
+    };
+
+    $scope.$on('$stateChangeSuccess', function () {
+      $scope.loadMore();
+    });
+  })
+
+  .controller('BoardCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  })
+  .controller('MypageCtrl', function ($scope, User) {
+    $scope.settings = {
+      enableFriends: true
+    };
+    $scope.logout = function () {
+      User.logout();
+    }
+  })
+
+  .controller('LoginCtrl', function ($scope, User, $ionicModal) {
+    $scope.login = function () {
+      $scope.openModal();
+    }
+    $scope.confirm=function(){
+      $scope.closeModal();
+      User.login();
+    }
+    $ionicModal.fromTemplateUrl('templates/modal/login-join.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal_join = modal;
+    });
+
+    $scope.openModal = function (type) {
+      $scope.modal_join.show();
+    };
+    $scope.closeModal = function (type) {
+      $scope.modal_join.hide();
+    };
+  })
+
