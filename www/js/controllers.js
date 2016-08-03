62225e26c087f53f6fb3baa3e6ec52bba46904dc
Woohyun Kim <r45r54r45@naver.com>
2016-08-03 16:21:35 +0900

    update on class-add

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-  .controller('ClassCtrl', function ($scope, $ionicModal, $ionicLoading) {
+  .controller('ClassCtrl', function ($scope, $ionicModal, $ionicLoading,$ionicPopup,$state) {
     $ionicModal.fromTemplateUrl('templates/modal/class-sort.html', {
       scope: $scope,
       animation: 'slide-in-up'
@@ -29,18 +29,39 @@ angular.module('starter.controllers', [])
         $scope.modal_add.hide();
       }
     };
-    $scope.showLoading = function () {
-      $ionicLoading.show({
-        template: 'Loading...'
-      }).then(function () {
-        console.log("The loading indicator is now displayed");
+    /*
+     open class popup
+     */
+    $scope.openClass = function(classNum) {
+      var confirmPopup = $ionicPopup.confirm({
+        title: '수업 제목',
+        template: '25포인트가 소모됩니다. 강의 평가를 확인하시겠습니까?'
+      });
+      confirmPopup.then(function(res) {
+        if(res) {
+          location.href="#/tab/class/"+classNum;
+        } else {
+          openClassNoPoint();
+        }
       });
     };
-    $scope.hideLoading = function () {
-      $ionicLoading.hide().then(function () {
-        console.log("The loading indicator is now hidden");
+    var openClassNoPoint = function() {
+      var alertPopup = $ionicPopup.alert({
+        template: '포인트가 부족합니다.'
       });
     };
+    // $scope.showLoading = function () {
+    //   $ionicLoading.show({
+    //     template: 'Loading...'
+    //   }).then(function () {
+    //     console.log("The loading indicator is now displayed");
+    //   });
+    // };
+    // $scope.hideLoading = function () {
+    //   $ionicLoading.hide().then(function () {
+    //     console.log("The loading indicator is now hidden");
+    //   });
+    // };
 
     $scope.priceSlider = {
       value: 0,
@@ -55,6 +76,59 @@ angular.module('starter.controllers', [])
       }
     };
 
+    /*
+    textarea logic
+     */
+    $scope.countOf = function(text) {
+      var s = text ? text.split(/\s+/) : 0; // it splits the text on space/tab/enter
+      var count= s ? s.length : '';
+      return count;
+    };
+
+    /*
+     checkbox logic
+      */
+    var checkboxCountAlert = function() {
+      var alertPopup = $ionicPopup.alert({
+        template: '선택된 태그가 너무 많습니다. <br>신중하게 선택해주세요 '
+      });
+    };
+    $scope.checkboxResult=[];
+    $scope.checkboxList=["c1","c2","c3"];
+    $scope.checkCheckboxCount=function(i){
+      var count=0;
+      if($scope.checkboxResult[i]==false){
+        return;
+      }
+      $scope.checkboxResult.forEach(function(item,index){
+        if(item==true){
+          if(count+1>2){
+            checkboxCountAlert();
+            $scope.checkboxResult[i]=false;
+          }else{
+            count++;
+          }
+        }
+      });
+    }
+
+
+    /*
+     submit popup
+      */
+    $scope.submit = function() {
+      var confirmPopup = $ionicPopup.confirm({
+        title: '수업평가',
+        template: '수업 평가를 등록하시겠습니까?'
+      });
+      confirmPopup.then(function(res) {
+        if(res) {
+          console.log('You are sure');
+        } else {
+          console.log('You are not sure');
+        }
+      });
+    };
 
   })
   .controller('NoticeCtrl', function ($scope) {
