b13cfb1bdc01c07e46603094829defb987203bf6
Jaeha Lee <jhl.webproduction@gmail.com>
2016-08-14 01:58:04 +0900

    keyboard

--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,7 @@ angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers',
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
       }
       if (window.StatusBar) {
